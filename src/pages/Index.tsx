21ef297e70abc081d2bf3c08a1ba461b21b6d275
roomit-app <roomlyusc@gmail.com>
2026-01-03 00:31:18 +0100

    foto_hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import About from "@/components/About";
 import Gallery from "@/components/Gallery";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-
+import { Analytics } from "@vercel/analytics/next"
 const Index = () => {
   return (
     <div className="min-h-screen">
