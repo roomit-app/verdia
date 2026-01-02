3c77e1c313cb2cd052ef124c6aa1a6670263c513
roomit-app <roomlyusc@gmail.com>
2026-01-02 19:21:37 +0100

    video_hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Leaf } from "lucide-react";
-import heroVideo from "@/assets/hero-video.mp4";
+import heroVideo from "@/assets/hero.mp4";
 
 const Hero = () => {
   return (
